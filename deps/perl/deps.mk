f32680f107195215bec0c12f2f34809a710c3fda
Jan Tusil <jenda.tusil@gmail.com>
2018-12-15 15:07:16 -0600

    --self-contained

--- deps/perl/deps.mk
+++ deps/perl/deps.mk
@@ -1,2 +1,2 @@
-CPANM := $(CPANM_EXECUTABLE) --local-lib=$(BUILD_LOCAL_PERL)
+CPANM := $(CPANM_EXECUTABLE) --self-contained --local-lib=$(BUILD_LOCAL_PERL)
 export PERL5LIB := $(BUILD_LOCAL_PERL)/lib/perl5:$(PERL5LIB)
