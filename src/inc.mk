b0389533d11f0fa1b09ac9299f9a6a987471330a
Jan Tusil <jenda.tusil@gmail.com>
2018-12-16 19:21:40 -0600

    Group by profile #1

--- src/inc.mk
+++ src/inc.mk
@@ -6,8 +6,14 @@ ifeq ($(strip $(BUILD_ROOT)),)
   $(error Need BUILD_ROOT)
 endif
 
+ifeq ($(strip $(DIST_ROOT)),)
+  $(error Need DIST_ROOT)
+endif
+
 B := $(BUILD_DIR)
 
 GENERATING = echo Creating $(@:$(BUILD_ROOT)/%=%)
+ECHO_OPEN  = echo "< $@>"
+ECHO_CLOSE = echo "</$@>"
 
 include $(BUILD_ROOT)/deps/deps.mk
